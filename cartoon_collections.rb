2e17c37302122d58f9d21644d5324f84e4348d58
Amirata K <amiratak88@gmail.com>
2018-07-13 01:15:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,9 @@ def roll_call_dwarves# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)
   # Your code here
+  
 end
 
 def long_planeteer_calls# code an argument here
